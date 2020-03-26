112d620926fc54da1576b96c057b261201e09c0b
Saksham Gupta <sakshamgupta05@gmail.com>
2020-03-27 01:09:59 +0530

    initial commit

---
+++ lib/exsm.ex
@@ -0,0 +1,18 @@
+defmodule Exsm do
+  @moduledoc """
+  Documentation for Exsm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exsm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exsm_test.exs
@@ -0,0 +1,8 @@
+defmodule ExsmTest do
+  use ExUnit.Case
+  doctest Exsm
+
+  test "greets the world" do
+    assert Exsm.hello() == :world
+  end
+end
